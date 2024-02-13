f862ae469b046725db79e9242d532d7f42b6d128
IsabellaSulisufi <114592322+IsabellaSulisufi@users.noreply.github.com>
2024-02-13 15:22:52 +0000

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Isabella Sulisufi on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
