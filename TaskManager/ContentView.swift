59f485d17bfc5c2499e6f189cf8fdfa8eb67af17
IsabellaSulisufi <114592322+IsabellaSulisufi@users.noreply.github.com>
2024-02-20 11:30:33 +0000

    initialise repo

--- TaskManager/ContentView.swift
+++ TaskManager/ContentView.swift
@@ -1,19 +1,19 @@
-//
+//ReportCorrectionsEmptyViewDelegate
 //  ContentView.swift
 //  TaskManager
 //
 //  Created by Isabella Sulisufi on 13/02/2024.
-//
+
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            Image("LogoImage")
+                .imageScale(.small)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Test")
         }
         .padding()
     }
